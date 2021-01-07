be8edf9a3b8c672d3d3d0fb5f5f0d5c7d3d02fcb
Scalfi <gascalfi@gmail.com>
2021-01-07 20:45:53 -0300

    create model sickness and customerskciness

---
+++ CalendarPerfect/Models/Database/CustomerSickness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarPerfect.Models.Database
+{
+    public class CustomerSickness : BaseEntity
+    {
+        [ForeignKey(nameof(Sickness))]
+        [Column("sickness_id")]
+        public int SicknessId { get; set; }
+
+        public HashSet<Sickness> Sicknesses { get; set; }
+
+        [ForeignKey(nameof(Customer))]
+        [Column("customer_id")]
+        public int CustomerId { get; set; }
+        public List<Customer> Customeres { get; set; }
+    }
+}
---
+++ CalendarPerfect/Models/Database/Sickness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarPerfect.Models.Database
+{
+    [Table("sickness")]
+    public class Sickness : BaseEntity
+    {
+        [Column("description")]
+        public string Description { get; set; }
+    }
+}
