49a8dd9adc962509acbeef1cae3171d39dfbbf61
Scalfi <gascalfi@gmail.com>
2021-01-06 21:59:43 -0300

    create baseentity model and change version .net 3.1 > 5.0

---
+++ CalendarPerfect/Models/Database/Generics/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarPerfect.Models.Database
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
