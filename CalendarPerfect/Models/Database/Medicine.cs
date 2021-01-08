bdba9c6ecafe9d7ec1c851426b275fa86ea625b6
Scalfi <gascalfi@gmail.com>
2021-01-07 21:01:11 -0300

    create medicine model

---
+++ CalendarPerfect/Models/Database/Medicine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarPerfect.Models.Database
+{
+    [Table("medicine")]
+    public class Medicine : BaseEntity
+    {
+        [Column("description")]
+        public string Description { get; set; }
+
+        [ForeignKey(nameof(SicknessId))]
+        [Column("sickness_id")]
+        public int SicknessId { get; set; }
+
+        public Sickness Sickness { get; set; }
+    }
+}
