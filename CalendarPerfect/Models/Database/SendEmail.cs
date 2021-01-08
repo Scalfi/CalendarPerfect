e46f81d831d990e66f9d9aef64f1ef317610b49a
Scalfi <gascalfi@gmail.com>
2021-01-07 21:15:28 -0300

    create sendemail models

---
+++ CalendarPerfect/Models/Database/SendEmail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarPerfect.Models.Database
+{
+    [Table("send_emails")]
+    public class SendEmail : BaseEntity
+    {
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("attach")]
+        public string Attach { get; set; }
+
+        [Column("send_for_email")]
+        public string SendForEmail { get; set; }
+    }
+}
