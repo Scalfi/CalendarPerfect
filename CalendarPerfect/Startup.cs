9c981e1b4d9dc46728ffc2f43bcc318b8caef5ea
Scalfi <gascalfi@gmail.com>
2021-01-11 21:37:55 -0300

    create conection string

--- CalendarPerfect/Startup.cs
+++ CalendarPerfect/Startup.cs
@@ -1,7 +1,9 @@
+using CalendarPerfect.Models.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace CalendarPerfect
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<PostgresContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("CalendarPerfectContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
